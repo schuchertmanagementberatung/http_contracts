0c1d21a56cf3a538607b84cfbda921d25765e014
Heiko Mathes <heiko.mathes@gmail.com>
2017-09-08 20:17:47 +0200

    :bug: fix gulptraum usage

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,24 @@
 'use strict';
 
 const gulptraum = require('gulptraum');
+const gulptraumTypescriptPlugin = require('gulptraum-typescript');
+const tsconfig = require('tsconfig');
 
 const buildSystemConfig = {
-  packageName: 'http'
 };
 
 const buildSystem = new gulptraum.BuildSystem(buildSystemConfig);
 
 buildSystem.config = buildSystemConfig;
 
-const typeScriptConfig = {
-  compileToModules: ['commonjs'],
-};
+const tsConfigObj = tsconfig.loadSync('.');
+
+const typeScriptConfig = Object.assign({
+  compileToModules: ['commonjs']
+}, tsConfigObj.config);
 
 const gulp = require('gulp');
 
 buildSystem
-  .registerPlugin('typescript', gulptraum.plugins.typescript, typeScriptConfig)
+  .registerPlugin('typescript', gulptraumTypescriptPlugin, typeScriptConfig)
   .registerTasks(gulp);
