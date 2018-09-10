b674738c40abf4a3706125a198bd8d0131caab7f
5ebastianMeier <sebastian.meier@5minds.de>
2018-09-10 17:12:54 +0200

    add interface for http socket endpoint

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export * from './http_request_with_identity';
 export * from './ihttp_client';
 export * from './ihttp_extension';
 export * from './ihttp_router';
+export * from './ihttp_socket_endpoint';
 export * from './irequest_options';
 export * from './iresponse';
