60c6016b8bf28393554791bd7c2cdead27aec376
5ebastianMeier <sebastian.meier@5minds.de>
2018-09-12 09:25:28 +0200

    add constant for default socket namespace

---
+++ src/constants.ts
@@ -0,0 +1,1 @@
+export const DefaultSocketNamespace: string = '/default';
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export * from './constants';
 export * from './http_request_with_identity';
 export * from './ihttp_client';
 export * from './ihttp_extension';
