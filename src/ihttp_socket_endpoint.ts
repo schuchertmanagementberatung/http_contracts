0e5f788ef9227a26e643e277931ff216bba902c7
Sebastian Meier <sebastian.meier@5minds.de>
2019-03-20 17:39:35 +0100

    update endpoint name

--- src/ihttp_socket_endpoint.ts
+++ src/ihttp_socket_endpoint.ts
@@ -2,6 +2,6 @@ import {IEndpointSocketScope} from '@essential-projects/websocket_contracts';
 
 export interface IHttpSocketEndpoint {
   readonly namespace: string;
-  initializeEndpoint(socketIo: IEndpointSocketScope): Promise<any> | any;
+  initializeEndpoint(endpoint: IEndpointSocketScope): Promise<any> | any;
   dispose(): Promise<void> | void;
 }
